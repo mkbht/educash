0e7c09c46b4a241ac355153fbe1f2b30d664dd20
Mukunda Bhattarai <mkdbht@gmail.com>
2016-05-26 20:01:36 +0545

    Update home.php

--- home.php
+++ home.php
@@ -1,114 +1,134 @@
 <?php
-	$title = "EduCash Home";
+	$title = "Welcome to EduCash";
 	include 'inc/header.php';
 ?>
-<div class="well">
-	<div class="container">
-		<b>EduCash Home: Most Recents</b>
-	</div>
-</div>
-<div class="container home">
-	<div class="row">
-		<div class="col-md-9">
-				<!-- grid -->
-			<div class="col-md-6">
-				<div class="thumbnail">
-					<img src="img/books.jpg" class="img-responsive img-thumbnail">
-					<a href="content.php">
-						<span class="post-title">Lorem Ipsum Dolor lorem</span>
-					</a>
-					<small class="post-time">Jan 5, 2013 - 05:31 PM, Author: <a href="#">mkbht</a></small>
-					<span class="post-content">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ....
-					</span>
-				</div>
-			</div>
-		<!-- grid -->
-			<div class="col-md-6">
-				<div class="thumbnail">
-					<img src="img/teaching.jpg" class="img-responsive img-thumbnail">
-					<a href="content.php">
-						<span class="post-title">Lorem Ipsum Dolor lorem</span>
-					</a>
-					<small class="post-time">Jan 5, 2013 - 05:31 PM, Author: <a href="#">mkbht</a></small>
-					<span class="post-content">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ....
-					</span>
-				</div>
-			</div>
-		<!-- grid -->
-			<div class="col-md-6">
-				<div class="thumbnail">
-					<img src="img/kits.jpg" class="img-responsive img-thumbnail">
-					<a href="content.php">
-						<span class="post-title">Lorem Ipsum Dolor lorem</span>
-					</a>
-					<small class="post-time">Jan 5, 2013 - 05:31 PM, Author: <a href="#">mkbht</a></small>
-					<span class="post-content">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ....
-					</span>
-				</div>
-			</div>
-		<!-- grid -->
-			<div class="col-md-6">
-				<div class="thumbnail">
-					<img src="img/bannercopy.jpg" class="img-responsive img-thumbnail">
-					<a href="content.php">
-						<span class="post-title">Lorem Ipsum Dolor lorem</span>
-					</a>
-					<small class="post-time">Jan 5, 2013 - 05:31 PM, Author: <a href="#">mkbht</a></small>
-					<span class="post-content">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ....
-					</span>
-				</div>
-			</div>
-		<!-- grid -->
-			<div class="col-md-6">
-				<div class="thumbnail">
-					<img src="img/books.jpg" class="img-responsive img-thumbnail">
-					<a href="content.php">
-						<span class="post-title">Lorem Ipsum Dolor lorem</span>
-					</a>
-					<small class="post-time">Jan 5, 2013 - 05:31 PM, Author: <a href="#">mkbht</a></small>
-					<span class="post-content">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ....
-					</span>
-				</div>
-			</div>
-		<!-- grid -->
-			<div class="col-md-6">
-				<div class="thumbnail">
-					<img src="img/banner.jpg" class="img-responsive img-thumbnail">
-					<a href="content.php">
-						<span class="post-title">Lorem Ipsum Dolor lorem</span>
-					</a>
-					<small class="post-time">Jan 5, 2013 - 05:31 PM, Author: <a href="#">mkbht</a></small>
-					<span class="post-content">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ....
-					</span>
-				</div>
-			</div>
 
-			<!-- paging -->
-			<ul class="pager">
-			  <li class="previous disabled"><a href="#">Previous</a></li>
-			  <li class="next"><a href="#">Next</a></li>
-			</ul>
+<div class="banner">
+	<div class="col-md-offset-3 col-md-6">
+		<h1 class="title">LOREM IPSUM DOLOR</h1>
+		<p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+	</div>
+	<button class="btn-explore">JOIN US TODAY</button>
+</div>
+
+
+<div class="motto">
+			<div class="container">
+				<div class="col-md-4">
+					<center>
+						<i class="fa fa-book fa-5x"></i>
+						<p class="block">LEARN</p>
+					</center>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco dolor lorem laboris nisi.
+					</p>
+				</div>
+				<div class="col-md-4">
+				<center>
+						<i class="fa fa-share-alt fa-5x"></i>
+						<p class="block">SHARE</p>
+					</center>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco dolor lorem laboris nisi.
+					</p>
+				</div>
+				<div class="col-md-4">
+					<center>
+						<i class="fa fa-dollar fa-5x"></i>
+						<p class="block">EARN</p>
+					</center>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco dolor lorem laboris nisi.
+					</p>
+				</div>
+			</div>
 		</div>
 
-		<?php include('inc/sidebar.php'); ?>
-	</div>
-</div>
-<?php include 'inc/footer.php' ?>
+		<div class="featured">
+			<div class="container">
+			<center><h1>TRENDING</h1></center>
+			<br>
+				<div class="col-md-3">
+					<div class="card">
+						<img src="img/book.jpg" class="img-responsive">
+					<div class="content">
+						<h5>Lorem ipsum dolor sit amet,</h5>
+						 consectetur adipisicing elit,consectetur adipisicing elit, sed do eiusmod
+						temporadipisicing elit,consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut ...
+						<a href="content.php"><button class="btn btn-block btn-default btn-black">View</button></a>
+					</div>
+					</div>
+				</div>
+				
+				
+				<div class="col-md-3">
+					<div class="card">
+						<img src="img/lappy.png" class="img-responsive">
+					<div class="content">
+						<h5>Lorem ipsum dolor sit amet,</h5>
+						 consecteturadipisicing elit,consectetur adipisicing elit, sed do eiusmod
+						temporadipisicing elit,consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut ...
+						<a href="content.php"><button class="btn btn-block btn-default btn-black">View</button></a>
+					</div>
+					</div>
+				</div>
+				
+				
+				<div class="col-md-3">
+					<div class="card">
+						<img src="img/banner.jpg" class="img-responsive">
+					<div class="content">
+						<h5>Lorem ipsum dolor sit amet,</h5>
+						 consectetur adipisicing elit,consectetur adipisicing elit, sed do eiusmod
+						temporadipisicing elit,consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut ...
+						<a href="content.php"><button class="btn btn-block btn-default btn-black">View</button></a>
+					</div>
+					</div>
+				</div>
+				
+				
+				<div class="col-md-3">
+					<div class="card">
+						<img src="img/kits.jpg" class="img-responsive">
+					<div class="content">
+						<h5>Lorem ipsum dolor sit amet,</h5>
+						 consecadipisicing elit,consectetur adipisicing elit, sed do eiusmod
+						temportetur adipisicing elit,consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut ...
+						<a href="content.php"><button class="btn btn-block btn-default btn-black">View</button></a>
+					</div>
+					</div>
+				</div>
+				
+				<center><a href="home.php"><button class="btn btn-default" style="margin-top: 30px">Explore More</button></a></center>
+			</div>
+		</div>
+
+		<div class="views">
+			<div class="container">
+			<div class="col-md-offset-3 col-md-6">
+				<center>
+					<h2><b>User's View</b></h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation<br><span class="viewer">- John Doe</span>
+					</p>
+
+
+					<span class="dots"></span>
+					<span class="dots filled"></span>
+					<span class="dots"></span>
+				</center>
+			</div>
+				</div>
+		</div>
+<?php include('inc/footer.php') ?>
